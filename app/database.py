3b9f662990a4720af4019a180ac4b37704e476d8
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-08-07 18:50:04 +0800

    refactor: change db url to sqlite for dev and set env to dev in the entry point

--- app/database.py
+++ app/database.py
@@ -10,6 +10,12 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+    if DATABASE_URL.startswith("sqlite")
+    else {},
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 # app/main.py
 
+import os
 from fastapi import FastAPI
 
 from .database import Base, engine
 from .routers import services
 
-# Create tables
-Base.metadata.create_all(bind=engine)
+if os.getenv("ENV", "dev") == "dev":
+    Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(services.router)
