b244942fb12760259924d02f555fed95f3c0af69
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-08-07 11:18:15 +0800

    fix: 'orm_mode' has been renamed to 'from_attributes' in Pydantic v2

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class ServiceCreate(BaseModel):
     name: str
@@ -9,5 +9,4 @@ class ServiceOut(BaseModel):
     name: str
     url: str
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
